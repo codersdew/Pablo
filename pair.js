7db42291de04e19118d213d9eff45bc8de981abb
DEWCODERS <codersdew@gmail.com>
2026-03-09 19:52:22 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -94,7 +94,7 @@ const checkApiKey = async () => {
 // ---------------- MONGO SETUP ----------------
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://camalkaakash2_db_user:QVIRCgDpbjr2adcb@dtznovaxpaspapers.ddt0qup.mongodb.net/?appName=dtznovaxpaspapers';
-const MONGO_DB = process.env.MONGO_DB || 'KEZU-MD';
+const MONGO_DB = process.env.MONGO_DB || 'PABLO-MD';
 
 let mongoClient, mongoDB;
 let sessionsCol, numbersCol, adminsCol, newsletterCol, configsCol, newsletterReactsCol, followChannelsCol;
