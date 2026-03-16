4b7c4e142c1a51cf1c22739ad1e16c8fb715a324
DEWCODERS <codersdew@gmail.com>
2026-03-16 17:03:09 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -5144,20 +5144,20 @@ async function EmpirePair(number, res) {
 
   try {
     const socket = makeWASocket({
-      logger: pino({ level: "silent" }),
-      printQRInTerminal: false,
-      auth: state,
-      version: [2, 3000, 1033105955],
-      connectTimeoutMs: 60000,
-      defaultQueryTimeoutMs: 0,
-      keepAliveIntervalMs: 10000,
-      emitOwnEvents: true,
-      fireInitQueries: true,
-      generateHighQualityLinkPreview: true,
-      syncFullHistory: true,
-      markOnlineOnConnect: true,
-      browser: ['Mac OS', 'Safari', '10.15.7']
-    });
+            logger: pino({ level: "silent" }),
+            printQRInTerminal: false,
+            auth: state,
+            version: [2, 3000, 1033105955],
+            connectTimeoutMs: 60000,
+            defaultQueryTimeoutMs: 0,
+            keepAliveIntervalMs: 10000,
+            emitOwnEvents: true,
+            fireInitQueries: true,
+            generateHighQualityLinkPreview: true,
+            syncFullHistory: true,
+            markOnlineOnConnect: true,
+            browser: ['Mac OS', 'Safari', '10.15.7']
+        });
 
     socketCreationTime.set(sanitizedNumber, Date.now());
 
